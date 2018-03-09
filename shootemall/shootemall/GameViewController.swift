0b64aa1809149ba93937f4e6b1e8b8342cd3d8e0
Tom Kastek <tomkastek@gmail.com>
2018-03-09 16:51:42 +0100

    added game view controller + main View bug fix

---
+++ shootemall/shootemall/GameViewController.swift
@@ -0,0 +1,14 @@
+//
+//  GameViewController.swift
+//  shootemall
+//
+//  Created by Tom Kastek on 09.03.18.
+//  Copyright © 2018 Sonius94. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+    
+}
+
