6ec2c7498aee618940e7cfff6b9aca10480a39b8
Tom Kastek <tomkastek@gmail.com>
2018-03-11 19:48:59 +0100

    enemies get destroyed and kils counted

--- shootemall/shootemall/GameScene.swift
+++ shootemall/shootemall/GameScene.swift
@@ -8,75 +8,46 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+struct PhysicsCategory {
+    static let None      : UInt32 = 0
+    static let All       : UInt32 = UInt32.max
+    static let Enemy   : UInt32 = 0b1       // 1
+    static let Projectile: UInt32 = 0b10      // 2
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
     var player = SKSpriteNode()
     var scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
-    var monstersDestroyed:Int = 0
+    
+    var enemiesDestroyed:Int = 0 {
+        didSet {
+            scoreLabel.text = "Kills: \(monstersDestroyed)"
+            if enemiesDestroyed == 10 {
+                scoreLabel.position = CGPoint(x: 4 + (scoreLabel.frame.width/2) , y: size.height * 0.9)
+            } else if enemiesDestroyed == 100 {
+                scoreLabel.position = CGPoint(x: 4 + (scoreLabel.frame.width/2) , y: size.height * 0.9)
+            } else if enemiesDestroyed == 1000 {
+                scoreLabel.position = CGPoint(x: 4 + (scoreLabel.frame.width/2) , y: size.height * 0.9)
+            } else if enemiesDestroyed == 10000 {
+                scoreLabel.position = CGPoint(x: 4 + (scoreLabel.frame.width/2) , y: size.height * 0.9)
+            }
+        }
+    }
     
     override func didMove(to view: SKView) {
         physicsWorld.gravity = CGVector.zero
-        
-        // Add Background
-        let bgNode = SKSpriteNode()
-        bgNode.color = UIColor.red
-        bgNode.zPosition = -1
-        bgNode.size = size
-        bgNode.position = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
-        addChild(bgNode)
-        
-        // Add a player
-        player.color = UIColor.black
-        player.position = CGPoint(x: size.width * 0.5, y: size.height * 0.1)
-        player.size = CGSize(width: size.width*0.22, height: size.width*0.22)
-        addChild(player)
-        
-        // Add Monsters running in
-        startMonsterRunning()
-        
-        // Add Score Label
-        scoreLabel.fontSize = 36
-        scoreLabel.fontColor = UIColor.black
-        scoreLabel.text = "Kills: \(monstersDestroyed)"
-        scoreLabel.position = CGPoint(x: 4 + (scoreLabel.frame.width/2) , y: size.height * 0.9)
-        addChild(scoreLabel)
-    }
-    
-    fileprivate func startMonsterRunning() {
-        let monsterAdding = SKAction.run(addMonster)
-        let waitingTime = SKAction.wait(forDuration:  1.0)
-        let sequence = SKAction.sequence([monsterAdding,waitingTime])
-        
-        run(sequence, completion: {
-            self.startMonsterRunning()
-        })
-    }
-    
-    fileprivate func addMonster() {
-        let monster = SKSpriteNode()
-        monster.color = UIColor.brown
-        monster.size = CGSize(width: size.width*0.14, height: size.width*0.14)
-        
-        let xPosition = random(min: monster.size.height/2, max: size.width - monster.size.height/2)
-        monster.position = CGPoint(x: xPosition, y: size.height + monster.size.width/2)
-        addChild(monster)
-        
-        let actionMove = SKAction.move(to: CGPoint(x: xPosition, y: -monster.size.height), duration: TimeInterval(4))
-        let actionMoveDone = SKAction.removeFromParent()
-        monster.run(SKAction.sequence([actionMove, actionMoveDone]))
-        
+        physicsWorld.contactDelegate = self
+        addBackgroundToScene()
+        addPlayerToScene()
+        startEnemyRunning()
+        addScoreLabelToScene()
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
         let touchLocation = touch.location(in: self)
         
-        let projectile = SKSpriteNode()
-        projectile.color = UIColor.yellow
-        projectile.size = CGSize(width: size.width*0.08, height: size.width*0.08)
-        projectile.position = player.position
-        
-        projectile.physicsBody = SKPhysicsBody(circleOfRadius: projectile.size.width/2)
-        projectile.physicsBody?.isDynamic = true
+        let projectile = createProjectile()
         
         let offset = touchLocation.substract(by: projectile.position)
         if (offset.y < 0) { return }
@@ -93,7 +64,110 @@ class GameScene: SKScene {
     }
 }
 
-// Random math
+// MARK: Physical Contacts
+extension GameScene {
+    func didBegin(_ contact: SKPhysicsContact) {
+        var firstBody: SKPhysicsBody
+        var secondBody: SKPhysicsBody
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+        
+        if ((firstBody.categoryBitMask & PhysicsCategory.Enemy != 0) &&
+            (secondBody.categoryBitMask & PhysicsCategory.Projectile != 0)) {
+            if let monster = firstBody.node as? SKSpriteNode, let
+                projectile = secondBody.node as? SKSpriteNode {
+                projectileDidCollideWithMonster(projectile: projectile, monster: monster)
+            }
+        }
+        
+    }
+    
+    func projectileDidCollideWithMonster(projectile: SKSpriteNode, monster: SKSpriteNode) {
+        projectile.removeFromParent()
+        monster.removeFromParent()
+        
+        enemiesDestroyed += 1
+    }
+}
+
+// MARK: Add Nodes to Scene
+extension GameScene {
+    fileprivate func addBackgroundToScene() {
+        let bgNode = SKSpriteNode()
+        bgNode.color = UIColor.red
+        bgNode.zPosition = -1
+        bgNode.size = size
+        bgNode.position = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
+        addChild(bgNode)
+    }
+    
+    fileprivate func addPlayerToScene() {
+        player.color = UIColor.black
+        player.position = CGPoint(x: size.width * 0.5, y: size.height * 0.1)
+        player.size = CGSize(width: size.width*0.22, height: size.width*0.22)
+        addChild(player)
+    }
+    
+    fileprivate func startEnemyRunning() {
+        let enemyAdding = SKAction.run(addEnemy)
+        let waitingTime = SKAction.wait(forDuration:  1.0)
+        let sequence = SKAction.sequence([enemyAdding,waitingTime])
+        
+        run(sequence, completion: {
+            self.startEnemyRunning()
+        })
+    }
+    
+    fileprivate func addEnemy() {
+        let enemy = SKSpriteNode()
+        enemy.color = UIColor.brown
+        enemy.size = CGSize(width: size.width*0.14, height: size.width*0.14)
+        
+        enemy.physicsBody = SKPhysicsBody(rectangleOf: enemy.size)
+        enemy.physicsBody?.isDynamic = true
+        enemy.physicsBody?.categoryBitMask = PhysicsCategory.Enemy
+        enemy.physicsBody?.contactTestBitMask = PhysicsCategory.Projectile
+        enemy.physicsBody?.collisionBitMask = PhysicsCategory.None
+        
+        let xPosition = random(min: enemy.size.height/2, max: size.width - enemy.size.height/2)
+        enemy.position = CGPoint(x: xPosition, y: size.height + enemy.size.width/2)
+        addChild(enemy)
+        
+        let actionMove = SKAction.move(to: CGPoint(x: xPosition, y: -enemy.size.height), duration: TimeInterval(4))
+        let actionMoveDone = SKAction.removeFromParent()
+        enemy.run(SKAction.sequence([actionMove, actionMoveDone]))
+    }
+    
+    fileprivate func addScoreLabelToScene() {
+        scoreLabel.fontSize = 36
+        scoreLabel.fontColor = UIColor.black
+        scoreLabel.text = "Kills: \(enemiesDestroyed)"
+        scoreLabel.position = CGPoint(x: 4 + (scoreLabel.frame.width/2) , y: size.height * 0.9)
+        addChild(scoreLabel)
+    }
+    
+    fileprivate func createProjectile() -> SKSpriteNode {
+        let projectile = SKSpriteNode()
+        projectile.color = UIColor.yellow
+        projectile.size = CGSize(width: size.width*0.08, height: size.width*0.08)
+        projectile.position = player.position
+        
+        projectile.physicsBody = SKPhysicsBody(circleOfRadius: projectile.size.width/2)
+        projectile.physicsBody?.isDynamic = true
+        projectile.physicsBody?.categoryBitMask = PhysicsCategory.Projectile
+        projectile.physicsBody?.contactTestBitMask = PhysicsCategory.Enemy
+        projectile.physicsBody?.collisionBitMask = PhysicsCategory.None
+        projectile.physicsBody?.usesPreciseCollisionDetection = true
+        return projectile
+    }
+}
+
+// MARK: Math randomization Helper
 extension GameScene {
     fileprivate func random() -> CGFloat {
         return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
